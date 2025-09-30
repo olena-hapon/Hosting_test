a22d534175434aa0fbb121f5242c19f20fc2694d
Olena Hapon <olenagapon90gmail.com>
2025-09-30 08:34:45 +0300

    Changes

--- src/js/showMore.js
+++ src/js/showMore.js
@@ -1,29 +1,34 @@
-function initShowMore(containerSelector, textSelector, buttonSelector, lines = 2) {
-  const container = document.querySelector(containerSelector);
-  if (!container) return;
+function initShowMore(
+  containerSelector,
+  textSelector,
+  buttonSelector,
+  lines = 2
+) {
+  const containers = document.querySelectorAll(containerSelector);
+  if (!containers.length) return;
 
-  const text = container.querySelector(textSelector);
-  const btn = container.querySelector(buttonSelector);
+  containers.forEach((container) => {
+    const text = container.querySelector(textSelector);
+    const btn = container.querySelector(buttonSelector);
 
-  if (!text || !btn) return;
+    if (!text || !btn) return;
 
-  btn.addEventListener("click", () => {
-    const style = window.getComputedStyle(text);
-    const lineClamp = text.style.webkitLineClamp || style.webkitLineClamp;
+    btn.addEventListener("click", () => {
+      const style = window.getComputedStyle(text);
+      const lineClamp = text.style.webkitLineClamp || style.webkitLineClamp;
 
-    if (lineClamp === "unset" || lineClamp === "none") {
-      text.style.webkitLineClamp = lines;
-      btn.textContent = "Pokaż więcej";
-    } else {
-      text.style.webkitLineClamp = "unset";
-      btn.textContent = "Pokaż mniej";
-    }
+      if (lineClamp === "unset" || lineClamp === "none") {
+        text.style.webkitLineClamp = lines;
+        btn.textContent = "Pokaż więcej";
+      } else {
+        text.style.webkitLineClamp = "unset";
+        btn.textContent = "Pokaż mniej";
+      }
+    });
   });
 }
 
 export default function showMore() {
-
   initShowMore(".hero", ".hero__text", ".hero__show", 2);
-
   initShowMore(".range__text__container", ".range__text", ".range__show", 3);
 }
